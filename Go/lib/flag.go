56e4cca7775f7ffb50df26fe7a9c6fddc6fe3eba
KevinBlandy <747692844@qq.com>
2021-02-16 10:30:25 +0800

    updat

--- Go/lib/flag.go
+++ Go/lib/flag.go
@@ -1,41 +1,60 @@
 ---------------------------------------
 flag
 ---------------------------------------
+	# 支持的命令格式
+		-flag xxx （使用空格，一个-符号）
+		--flag xxx （使用空格，两个-符号）
+		-flag=xxx （使用等号，一个-符号）
+		--flag=xxx （使用等号，两个-符号）
+	
+	# Flag解析在第一个非flag参数（单个”-“不是flag参数）之前停止，或者在终止符”–“之后停止。
+
+	# 如果异常处理是不是：ContinueOnError
+		* 参数必须要先“定义”，然后才解析，不然会异常
+			flag provided but not defined: [参数名称]
 
 ---------------------------------------
 变量
 ---------------------------------------
 	var CommandLine = NewFlagSet(os.Args[0], ExitOnError)
+		* 默认的，就是命令行参数
+
 	var ErrHelp = errors.New("flag: help requested")
+		* help信息是必须的
+	
 	var Usage = func() {
 		fmt.Fprintf(CommandLine.Output(), "Usage of %s:\n", os.Args[0])
 		PrintDefaults()
 	}
+		* 默认的说明方法
 
 ---------------------------------------
 type
 ---------------------------------------
 	# type Flag struct {
-			Name     string // name as it appears on command line
-			Usage    string // help message
-			Value    Value  // value as set
-			DefValue string // default value (as text); for usage message
+			Name     string //参数名称
+			Usage    string // 帮助信息
+			Value    Value  // 值列表
+			DefValue string // 默认值
 		}
 		func Lookup(name string) *Flag
+			* 根据参数，检索指定的值Flag
 	
 	# type ErrorHandling int
 			const (
-			ContinueOnError ErrorHandling = iota // 返回异常异常信息
-			ExitOnError                          // Call os.Exit(2) or for -h/-help Exit(0).
-			PanicOnError                         // Call panic with a descriptive error.
+			ContinueOnError ErrorHandling = iota // 异常的时候继续执行
+			ExitOnError                          // 执行系统退出 os.Exit(2) 或者 for -h/-help Exit(0).
+			PanicOnError                         // panic
 		)
 
 		* 异常处理枚举
 		
 
 	# type FlagSet struct {
-			Usage func()
+			Usage func()		// 参数说明输出文档
 		}
+
+		* 保存了所有的参数
 		
 		func NewFlagSet(name string, errorHandling ErrorHandling) *FlagSet
 
@@ -59,6 +78,8 @@ type
 		func (f *FlagSet) Name() string
 		func (f *FlagSet) Output() io.Writer
 		func (f *FlagSet) Parse(arguments []string) error
+			* 解析参数
+		
 		func (f *FlagSet) Parsed() bool
 		func (f *FlagSet) PrintDefaults()
 		func (f *FlagSet) Set(name, value string) error
@@ -77,38 +98,52 @@ type
 			Value
 			Get() interface{}
 		}
+
+		* 参数值的转换接口，可以把参数值转换为 其他类型
+		* 它实现了 Value 接口
 	
 	# type Value interface {
-			String() string
-			Set(string) error
+			String() string			// 返回值
+			Set(string) error		// 重新设置值
 		}
+
+		* 参数值的接口
+
 
 ---------------------------------------
 方法
 ---------------------------------------
 	func Arg(i int) string
 	func Args() []string
+		* 返回未定义的参数列表
+
 	func Bool(name string, value bool, usage string) *bool
+	func Duration(name string, value time.Duration, usage string) *time.Duration
+	func Float64(name string, value float64, usage string) *float64
+	func Int64(name string, value int64, usage string) *int64
+	func Int(name string, value int, usage string) *int
 		* 参数名称为name，默认值为value，usage是提示信息
-		* 返回的这个参数值的引用
+		* 返回的这个参数值的指针
+		* 时间的解析可以是： 1m/1s/1h/2h45m 等等
 
 	func BoolVar(p *bool, name string, value bool, usage string)
-	func Duration(name string, value time.Duration, usage string) *time.Duration
 	func DurationVar(p *time.Duration, name string, value time.Duration, usage string)
-	func Float64(name string, value float64, usage string) *float64
 	func Float64Var(p *float64, name string, value float64, usage string)
-	func Int(name string, value int, usage string) *int
-	func Int64(name string, value int64, usage string) *int64
 	func Int64Var(p *int64, name string, value int64, usage string)
 	func IntVar(p *int, name string, value int, usage string)
+		* 自己设置值的指针
 
 	func NArg() int
 	func NFlag() int
+
 	func Parse()
 		* 通常，在定义了命令行选项后，需要调用flag.Parse()。
 
 	func Parsed() bool
+		* 返回，是否已经解析了参数
+
 	func PrintDefaults()
+
 	func Set(name, value string) error
 	func String(name string, value string, usage string) *string
 	func StringVar(p *string, name string, value string, usage string)
@@ -142,4 +177,39 @@ Demo
 		// main.exe -epoll=true -port=8080 -host=192.168.0.152
 		// epoll=true, port=8080, host=192.168.0.152
 	
+
+	# 解析自定义的参数
+		import (
+			"flag"
+			"log"
+			"time"
+		)
+		func main() {
+			var flagSet = flag.NewFlagSet("app启动参数", flag.ExitOnError)
+
+			// 预定义要读取的参数
+			var name = flagSet.String("name", "", "名字")
+			var age = flagSet.Int("age", 0, "年龄")
+			var expire = flagSet.Duration("expire", time.Second * 0, "vip过期时间")
+
+			// 解析参数
+			var err = flagSet.Parse([]string{"-name=Vin", "-age=28", "-expire=2h45m"})
+			// var err = flagSet.Parse([]string{"-name=Vin -age=28 expire=2h45m"})
+
+			if err != nil {
+				log.Println(err)
+			}
+
+			// 遍历所有预定义参数
+			flagSet.VisitAll(func(f *flag.Flag) {
+				log.Printf("name=%s, value=%s\n", f.Name, f.Value.String())
+			})
+			/*
+				2021/02/16 10:11:53 name=age, value=28
+				2021/02/16 10:11:53 name=expire, value=2h45m0s
+				2021/02/16 10:11:53 name=name, value=Vin
+			*/
+
+			log.Printf("name=%s, age=%d, expire=%s\n", *name, *age, *expire)		// 2021/02/16 10:11:53 name=Vin, age=28, expire=2h45m0s
+		}
 
--- Go/lib/path/filepath.go
+++ Go/lib/path/filepath.go
@@ -6,14 +6,16 @@ filepath
 变量
 -------------------------
 	const (
-		Separator     = os.PathSeparator
-		ListSeparator = os.PathListSeparator
+		Separator     = os.PathSeparator			// 路径分隔符（分隔路径元素）
+		ListSeparator = os.PathListSeparator		// 路径列表分隔符（分隔多个路径）
 	)
+		
+		* 跨平台的路径分隔符和路径集合分隔符
 
 	var ErrBadPattern = errors.New("syntax error in pattern")
 	
-	* 遍历目录的时候，可以返回这个异常，则跳出当前目录
-		var SkipDir = errors.New("skip this directory")
+	var SkipDir = errors.New("skip this directory")
+		* 遍历目录的时候，可以返回这个异常，则跳出当前目录
 
 
 -------------------------
@@ -40,33 +42,53 @@ type
 		* 可以用于把网络路径改成本地的文件路径
 			filepath.Clean("/dd/dd.jpg") // \dd\dd.jpg
 		* 空字符串，返回 "."
+		* 清理路径中的多余字符，比如 /// 或 ../ 或 ./
 
 	func Dir(path string) string
 		* 返回路径中除去最后一个路径元素的部分，即该路径最后一个元素所在的目录
 
 
 	func EvalSymlinks(path string) (string, error)
+		* 返回链接（快捷方式）所指向的实际文件
+
 	func Ext(path string) string
 		* 获取后缀名称
 	
 	func FromSlash(path string) string
+		* 将 path 中的 '/' 转换为系统相关的路径分隔符
+				
 	func Glob(pattern string) (matches []string, err error)
+		* 列出与指定的模式 pattern 完全匹配的文件或目录（匹配原则同上）
+
 	func HasPrefix(p, prefix string) bool
 	func IsAbs(path string) bool
 		* 返回路径是否是一个绝对路径
 	
 	func Join(elem ...string) string
 		* 拼接路径，如果路径中已经包含了一些多余的分隔符，会被去除
+		* 忽略空元素，清理多余字符。
 
 	func Match(pattern, name string) (matched bool, err error)
+		* name 是否和指定的模式 pattern 完全匹配
+	
 	func Rel(basepath, targpath string) (string, error)
+		* 获取 targpath 相对于 basepath 的路径。
+		* 要求 targpath 和 basepath 必须“都是相对路径”或“都是绝对路径”。
+
 	func Split(path string) (dir, file string)
 		* 把path分隔为目录和文件名两部分
 		* 如果没有目录，则 dir 是空字符串
 
-	func SplitList(path string) []string
+	func SplitList(path string) []string	
+		* 路径序列 path 分割为多条独立的路径
+
 	func ToSlash(path string) string
+		* 将 path 中平台相关的路径分隔符转换为 '/'
+
 	func VolumeName(path string) string
+		* 返回路径字符串中的卷名
+			indows 中的 `C:\Windows` 会返回 "C:"
+			Linux 中的 `//host/share/name` 会返回 `//host/share`
 
 	func Walk(root string, walkFn WalkFunc) error
 		* 遍历目录树，如果walkFn返回 SkipDir ，则跳出当前目录，继续遍历
