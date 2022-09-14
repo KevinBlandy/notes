479b43a675b1f2f5a0d8865d858abf7d36e63b4d
KevinBlandy <kevinblandy.cn@gmail.com>
2022-09-14 22:49:58 +0800

    update

--- Spring-boot/springboot-initializr.java
+++ Spring-boot/springboot-initializr.java
@@ -56,3 +56,37 @@ SpringBoot Initializr
 		}
 
 		#PROXY-END/
+	
+	# 默认代理
+		
+		#PROXY-START/
+
+		location ^~ /
+		{
+			proxy_pass http://localhost:8080;
+			proxy_set_header Host $host;
+			proxy_set_header X-Real-IP $remote_addr;
+			proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+			proxy_set_header REMOTE-HOST $remote_addr;
+			proxy_set_header Upgrade $http_upgrade;
+			proxy_set_header Connection $connection_upgrade;
+			# proxy_hide_header Upgrade;
+
+			add_header X-Cache $upstream_cache_status;
+
+			#Set Nginx Cache
+			
+			
+			set $static_fileZHbkGDhd 0;
+			if ( $uri ~* "\.(gif|png|jpg|css|js|woff|woff2)$" )
+			{
+				set $static_fileZHbkGDhd 1;
+				expires 12h;
+				}
+			if ( $static_fileZHbkGDhd = 0 )
+			{
+			add_header Cache-Control no-cache;
+			}
+		}
+
+		#PROXY-END/
