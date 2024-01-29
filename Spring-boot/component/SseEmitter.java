232afa614108584b78ba0e5d2b76b5b832bee44f
KevinBlandy <kevinblandy.cn@gmail.com>
2024-01-29 21:15:04 +0800

    update

--- Spring-boot/component/SseEmitter.java
+++ Spring-boot/component/SseEmitter.java
@@ -6,6 +6,15 @@ SseEmitter
 	# SSE
 		http://www.ruanyifeng.com/blog/2017/05/server-sent_events.html
 
+
+		* SSE 使用 HTTP 协议，现有的服务器软件都支持。WebSocket 是一个独立协议。
+		* SSE 属于轻量级，使用简单；WebSocket 协议相对复杂。
+		* SSE 默认支持断线重连，WebSocket 需要自己实现。
+		* SSE 一般只用来传送文本，二进制数据需要编码后传送，WebSocket 默认支持传送二进制数据。
+		* SSE 支持自定义发送的消息类型。
+
+
+
 	# 类结构体系
 		public class SseEmitter extends ResponseBodyEmitter 
 	
