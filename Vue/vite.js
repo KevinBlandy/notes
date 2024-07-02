27b866b00c39dba19b8bd946080282266fea3ed8
KevinBlandy <kevinblandy.cn@gmail.com>
2024-07-02 20:11:29 +0800

    update

---
+++ Vue/vite.js
@@ -0,0 +1,22 @@
+------------------------
+vite
+------------------------
+	# 创建 Vue
+		pnpm create vite
+	
+	# '@' alias 相对路径映射
+
+		import { defineConfig } from 'vite'
+		import vue from '@vitejs/plugin-vue'
+		import path from 'path'
+
+		// https://vitejs.dev/config/
+		export default defineConfig({
+		  plugins: [vue()],
+		  resolve: {
+			alias: {
+			  "@": path.resolve('./src'),
+			}
+		  }
+		});
+
