d95aaba38d970ff49a130fc43e569d821b88a6b6
KevinBlandy <747692844@qq.com>
2024-08-24 21:31:57 +0800

    update

--- Codings/HMac.java
+++ Codings/HMac.java
@@ -1,28 +1,38 @@
+package chatty.common.util;
+
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
+import java.util.Objects;
+
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
-import org.apache.commons.codec.binary.Hex;
-
-
-/**
- * 
- * Hamc
- * 
- */
-public class Hamc {
-
-	public static byte[] sha512(byte[] content, byte[] secret) {
+public class Hash {
+	
+	public static String sha256Hex(String content) {
+		return HexFormat.of().formatHex(sha256(content.getBytes()));
+	}
+	
+	public static byte[] sha256(byte[] content) {
+		Objects.requireNonNull(content);
 		try {
-			Mac sha512Hmac = Mac.getInstance("HmacSHA512");
-			SecretKeySpec keySpec = new SecretKeySpec(secret, "HmacSHA512");
-			sha512Hmac.init(keySpec);
-			return sha512Hmac.doFinal(content);
-		} catch (Exception e) {
+			return MessageDigest.getInstance("SHA256").digest(content);
+		} catch (NoSuchAlgorithmException e) {
 			throw new RuntimeException(e);
 		}
 	}
-
-	public static String sha512Hex(byte[] content, byte[] secret) {
-		return Hex.encodeHexString(sha512(content, secret));
+	
+	public static byte[] hmacSHA256(byte[] key, byte[] content) {
+		try {
+			 Mac mac = Mac.getInstance("HmacSHA256");
+	         mac.init(new SecretKeySpec(key, "HmacSHA256"));
+	         return mac.doFinal(content);
+		} catch (NoSuchAlgorithmException | InvalidKeyException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
+
+
