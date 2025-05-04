0c4f945bab29c0ddfe6c36fbc82c8f94ec1999d0
KevinBlandy <kevinblandy.cn@gmail.com>
2025-05-04 17:21:01 +0800

    update

---
+++ Vue/vue-app.js
@@ -0,0 +1,28 @@
+---------------------
+App
+---------------------
+
+	createApp()
+	createSSRApp()
+	app.mount()
+	app.unmount()
+	app.onUnmount()
+	app.component()
+	app.directive()
+	app.use()
+	app.mixin()
+	app.provide()
+	app.runWithContext()
+	app.version
+	app.config
+	app.config.errorHandler
+	app.config.warnHandler
+	app.config.performance
+	app.config.compilerOptions
+	app.config.globalProperties
+	app.config.optionMergeStrategies
+		* 设置全局属性
+			app.config.globalProperties.msg = 'hello'
+		
+	app.config.idPrefix
+	app.config.throwUnhandledErrorInProduction
