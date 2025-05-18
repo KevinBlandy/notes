b18a2f537dd9e43fa7f787aad95b7ef485ab0947
KevinBlandy <kevinblandy.cn@gmail.com>
2025-05-18 15:10:47 +0800

    update

--- Js/第三方库/axios/axios.js
+++ Js/第三方库/axios/axios.js
@@ -28,31 +28,68 @@ axios
 			 timeout: 5000
 		});
 	
+	# Promise 响应结构
+	
+		axios.get('/user/12345')
+			.then(function (response) {
+			console.log(response.data);
+			console.log(response.status);
+			console.log(response.statusText);
+			console.log(response.headers);
+			console.log(response.config);
+		});		
 
-----------------------
-异常处理
-----------------------
-	axios.get('/user/12345')
-	  .catch(function (error) {
-		if (error.response) {
-		  // 请求成功发出且服务器也响应了状态码，但状态代码超出了 2xx 的范围
-		  console.log(error.response.data);
-		  console.log(error.response.status);
-		  console.log(error.response.headers);
-		} else if (error.request) {
-		  // 请求已经成功发起，但没有收到响应
-		  // `error.request` 在浏览器中是 XMLHttpRequest 的实例，
-		  // 而在node.js中是 http.ClientRequest 的实例
-		  console.log(error.request);
-		} else {
-		  // 发送请求时出了点问题
-		  console.log('Error', error.message);
+		{
+				// `data` 由服务器提供的响应
+				data: {},
+
+				// `status` 来自服务器响应的 HTTP 状态码
+				status: 200,
+
+				// `statusText` 来自服务器响应的 HTTP 状态信息
+				statusText: 'OK',
+
+				// `headers` 是服务器响应头
+				// 所有的 header 名称都是小写，而且可以使用方括号语法访问
+				// 例如: `response.headers['content-type']`
+				headers: {},
+
+				// `config` 是 `axios` 请求的配置信息
+				config: {},
+
+				// `request` 是生成此响应的请求
+				// 在node.js中它是最后一个ClientRequest实例 (in redirects)，
+				// 在浏览器中则是 XMLHttpRequest 实例
+				request: {}
 		}
-		// 可以访问配置信息
-		console.log(error.config);
-		// 可以获取更多关于HTTP错误的信息。
-		console.log(error.toJSON());
-	  });
+		
+	
+
+	# 异常处理
+		
+		* 非 20x 响应，或者其他异常都会进入 catch 代码块
+	
+		axios.get('/user/12345')
+		  .catch(function (error) {
+			if (error.response) {
+			  // 请求成功发出且服务器也响应了状态码，但状态代码超出了 2xx 的范围
+			  console.log(error.response.data);
+			  console.log(error.response.status);
+			  console.log(error.response.headers);
+			} else if (error.request) {
+			  // 请求已经成功发起，但没有收到响应
+			  // `error.request` 在浏览器中是 XMLHttpRequest 的实例，
+			  // 而在node.js中是 http.ClientRequest 的实例
+			  console.log(error.request);
+			} else {
+			  // 发送请求时出了点问题
+			  console.log('Error', error.message);
+			}
+			// 可以访问配置信息
+			console.log(error.config);
+			// 可以获取更多关于HTTP错误的信息。
+			console.log(error.toJSON());
+		  });
 
 
 ----------------------
