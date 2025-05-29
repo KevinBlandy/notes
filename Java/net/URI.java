72f4071690af1053ef660d93d00b15bec9064730
KevinBlandy <kevinblandy.cn@gmail.com>
2025-05-29 18:32:19 +0800

    upda

---
+++ Java/net/URI.java
@@ -0,0 +1,13 @@
+-------------------
+URI
+-------------------
+	# 表示资源
+	
+	# 构造函数（颇多）
+
+		// 协议、凭证（用户名密码）、主机、端口、PATH、查询参数、锚点
+		new URI("http", null, "localhost", 1024, request.getRequestURI(), request.getQueryString(), null)
+	
+
+
+
