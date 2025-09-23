97acdf7a91123217b99dcfa8a590b1863a3f0d1e
KevinBlandy <kevinblandy.cn@gmail.com>
2025-09-23 23:05:23 +0800

    update

---
+++ Solidity/solidity-入门.sol
@@ -0,0 +1,14 @@
+---------------------------------
+Solidity
+---------------------------------
+	# 开源协议
+		
+		* 以注释形式定义在第一行
+	
+			// SPDX-License-Identifier: MIT
+		
+		* 不指定开源协议
+
+			// SPDX-License-Identifier: UNLICENSED
+		
+		
