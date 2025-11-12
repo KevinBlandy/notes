2d9536fdcbe8ca216a8d6f92beac1dc1a0b0bcd0
Yee <kevinblandy.cn@gmail.com>
2025-11-12 17:09:10 +0800

    update

---
+++ Solidity/solidity-汇编.sol
@@ -0,0 +1,13 @@
+---------------
+汇编
+---------------
+    # 即最底层的指令
+
+        * 参考
+            https://github.com/ethereum/go-ethereum/blob/master/core/vm/opcodes.go
+    
+    # 汇编代码块
+
+        assembly {
+            // 汇编代码
+        }
